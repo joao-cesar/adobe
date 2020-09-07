acf5c95135d6ffff8d9844222dfe80cc7ccf8a2a
joao-cesar <joao.cesar@minifliper.com>
2020-09-07 10:38:55 -0300

    mouse wheel navigation and animate on scroll

--- animate cc/html5_canvas/animate_on_scroll/index.js
+++ animate cc/html5_canvas/animate_on_scroll/index.js
@@ -2,30 +2,29 @@
 
 var p; // shortcut to reference prototypes
 var lib={};var ss={};var img={};
-lib.ssMetadata = [
-		{name:"index_atlas_", frames: [[0,0,1280,960],[0,962,882,352]]}
-];
+lib.ssMetadata = [];
 
 
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
 // symbols:
-
-
-
-(lib.CachedBmp_163 = function() {
-	this.initialize(ss["index_atlas_"]);
-	this.gotoAndStop(0);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.CachedBmp_162 = function() {
-	this.initialize(ss["index_atlas_"]);
-	this.gotoAndStop(1);
-}).prototype = p = new cjs.Sprite();
 // helper functions:
 
 function mc_symbol_clone() {
-	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop));
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop, this.reversed));
 	clone.gotoAndStop(this.currentFrame);
 	clone.paused = this.paused;
 	clone.framerate = this.framerate;
@@ -41,164 +40,313 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 	}
 
 
-(lib.Hit = function(mode,startPosition,loop) {
-	this.initialize(mode,startPosition,loop,{});
+(lib.Shapes = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
 
-	// hit
-	this.instance = new lib.CachedBmp_163();
-	this.instance.setTransform(-320,-240,0.5,0.5);
-	this.instance._off = true;
+	// shapes
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#FFFFFF").s().p("Ap/KAIAAz/IT/AAIAAT/g");
 
-	this.timeline.addTween(cjs.Tween.get(this.instance).wait(3).to({_off:false},0).wait(1));
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#FFFFFF").s().p("AnEHFQi7i7AAkKQAAkIC7i8QC8i7EIAAQEJAAC8C7QC7C8AAEIQAAEKi7C7Qi8C7kJAAQkIAAi8i7g");
+
+	this.shape_2 = new cjs.Shape();
+	this.shape_2.graphics.f("#FFFFFF").s().p("Ap/KAIJ/z/IKAT/g");
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape}]}).to({state:[{t:this.shape_1}]},1).to({state:[{t:this.shape_2}]},1).wait(1));
 
 	this._renderFirstFrame();
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(-320,-240,640,480);
+p.nominalBounds = new cjs.Rectangle(-64,-64,128,128);
 
 
-(lib.Bar1 = function(mode,startPosition,loop) {
-	this.initialize(mode,startPosition,loop,{});
+(lib.Background = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
 
-	// Capa_1
-	this.instance = new lib.CachedBmp_162();
-	this.instance.setTransform(0,0,0.5,0.5);
+	// Layer_1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#FFFFFF").s().p("EhK/AqMMAAAhUXMCV/AAAMAAABUXg");
+	this.shape.setTransform(480,270);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,960,540);
+
+
+(lib.TriangleAnimation = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// shape
+	this.instance = new lib.Shapes("single",2);
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
 
 	this._renderFirstFrame();
 
-}).prototype = getMCSymbolPrototype(lib.Bar1, new cjs.Rectangle(0,0,441,176), null);
+}).prototype = getMCSymbolPrototype(lib.TriangleAnimation, new cjs.Rectangle(-64,-64,128,128), null);
 
 
-// stage content:
-(lib.animate_cc_html5_canvas_animate_on_scroll = function(mode,startPosition,loop) {
-	this.initialize(mode,startPosition,loop,{});
+(lib.SquareAnimation = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
 
 	// timeline functions:
 	this.frame_0 = function() {
-		var root = this;
-		
-		root.targetTimeline = this;
-		root.targetTimeline.loop = true;
-		root.targetTimeline.force = 2;
-		root.targetTimeline.friction = 0.8;
-		root.targetTimeline.direction = -1; // scroll direction
-		root.targetTimeline.minFrame = 0; // set the start range value here
-		root.targetTimeline.maxFrame = root.targetTimeline.totalFrames - 1; // set the end range value here
-		root.targetTimeline.speed = 0;
-		root.targetTimeline.pressed = false;
-		
-		root.start = function ()
-		{
-			createjs.Touch.enable(stage);
-			root.hit.cursor = "default";
-			root.gotoAndStop(root.targetTimeline.minFrame);
-			canvas.addEventListener('mousewheel', root.onMouseWheel.bind(root));
-			canvas.addEventListener('DOMMouseScroll', root.onMouseWheel.bind(root));
-			stage.on("stagemousedown", root.onStageMouseDown.bind(root));
-			createjs.Ticker.on("tick", root.tickHandler);
-		};
-		
-		root.onMouseWheel = function (e)
-		{
-			e.preventDefault();
-		
-			var evt = window.event || e;
-			var delta = Math.max(-1, Math.min(1, evt.wheelDelta || -evt.detail));
-		
-			root.targetTimeline.speed = delta * root.force * root.direction;
-		};
-		
-		root.onStageMouseDown = function (e)
-		{
-			root.targetTimeline.pressedY = e.stageY / stage.scaleY;
-			root.targetTimeline.pressed = true;
-			root.targetTimeline.stageMouseUp = stage.on("stagemouseup", root.onStageMouseUp.bind(root));
-		};
-		
-		root.tickHandler = function (e)
-		{
-			var clamp = root.targetTimeline.loop ? "loopClamp" : "clamp";
-			var mouseY = stage.mouseY / stage.scaleY;
-			
-			if (root.targetTimeline.pressed && mouseY !== root.targetTimeline.pressedY)
-			{
-				root.targetTimeline.speed = (mouseY > root.targetTimeline.pressedY ? 1 : -1) * root.direction * root.force;
-				root.targetTimeline.pressedY = mouseY;
-			}
-			
-			root.targetTimeline.speed *= root.targetTimeline.friction;
-			root.targetTimeline.gotoAndStop(root[clamp](root.targetTimeline.currentFrame + root.targetTimeline.speed, root.targetTimeline.minFrame, root.targetTimeline.maxFrame));
-		};
-		
-		root.onStageMouseUp = function (e)
-		{
-			root.targetTimeline.pressed = false;
-			stage.off("stagemouseup", root.targetTimeline.stageMouseUp);
-		};
-		
-		root.loopClamp = function(value, min, max)
-		{
-			if (value < min)
-				return min;
-			
-			if (value > max)
-				return max;
-				
-			return value;
-		};
-		
-		root.clamp = function(value, min, max)
-		{
-			if (value < min)
-				return max;
-			
-			if (value > max)
-				return min;
-				
-			return value;
-		};
-		
-		if (!root.hasStarted)
-		{
-			root.start();
-			root.hasStarted = true;
-		}
+		this.stop();
+	}
+	this.frame_29 = function() {
+		this.stop();
 	}
 
 	// actions tween:
-	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(60));
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(29).call(this.frame_29).wait(1));
 
-	// hit
-	this.hit = new lib.Hit();
-	this.hit.name = "hit";
-	this.hit.setTransform(320,240);
-	new cjs.ButtonHelper(this.hit, 0, 1, 2, false, new lib.Hit(), 3);
+	// shape
+	this.instance = new lib.Shapes("single",0);
+	this.instance.setTransform(-544.05,0);
+	this.instance.alpha = 0;
 
-	this.timeline.addTween(cjs.Tween.get(this.hit).wait(60));
-
-	// anim
-	this.instance = new lib.Bar1();
-	this.instance.setTransform(657.95,152);
-
-	this.timeline.addTween(cjs.Tween.get(this.instance).to({x:-462.95},59,cjs.Ease.sineInOut).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.instance).to({x:0,alpha:1},29,cjs.Ease.quintOut).wait(1));
 
 	this._renderFirstFrame();
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(-142.9,240,1241.9,240);
+p.nominalBounds = new cjs.Rectangle(-608,-64,672,128);
+
+
+(lib.CircleAnimation = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// timeline functions:
+	this.frame_0 = function() {
+		this.stop();
+	}
+	this.frame_29 = function() {
+		this.stop();
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(29).call(this.frame_29).wait(1));
+
+	// shape
+	this.instance = new lib.Shapes("single",1);
+	this.instance.setTransform(0,-448.95);
+	this.instance.alpha = 0;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).to({y:0,alpha:1},29,cjs.Ease.backOut).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-64,-512.9,128,621.6999999999999);
+
+
+// stage content:
+(lib.animate_cc_html5_canvas_animate_on_scroll = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	this.actionFrames = [0];
+	this.isSingleFrame = false;
+	// timeline functions:
+	this.frame_0 = function() {
+		if(this.isSingleFrame) {
+			return;
+		}
+		if(this.totalFrames == 1) {
+			this.isSingleFrame = true;
+		}
+		var root = this;
+		
+		root.startPlaying = function(target)
+		{
+			target.gotoAndPlay(1);
+		};
+		
+		root.backToStart = function(target)
+		{
+			target.gotoAndStop(0);
+		};
+		
+		root.triangleReset = function(target)
+		{
+			target.alpha = 0;
+			target.rotation = -360;
+		};
+		
+		root.anims = 
+		[
+			{
+				target: root.square,
+				viewRatio: 0.75,
+				onEnter: root.startPlaying,
+				onExit: root.backToStart
+			},
+			{
+				target: root.circle,
+				viewRatio: 0.65,
+				onInit: null,
+				onEnter: function(target){target.gotoAndPlay(3);},
+				onExit: root.backToStart
+			},
+			{
+				target: root.triangle,
+				viewRatio: 0.75,
+				onInit: root.triangleReset,
+				onEnter: function(target){createjs.Tween.get(target).to({alpha:1, rotation:0}, 500, createjs.Ease.backOut);},
+				onExit: root.triangleReset
+			}
+		];
+		
+		root.scrollHandler = function(e)
+		{
+			var anim;
+			
+			for (var i = 0, total = root.anims.length; i < total; i++)
+			{
+				anim = root.anims[i];
+				
+				if (!anim.initialized && anim.onInit)
+				{
+					anim.initialized = true; 
+					anim.onInit(anim.target);
+				}			
+				
+				if (anim.started)
+				{			
+					if (window.scrollY <= anim.target.y - window.innerHeight)
+					{
+						anim.started = false;
+						
+						if (anim.onExit)
+							anim.onExit(anim.target);
+					}			
+				}
+				else
+				{			
+					if (window.scrollY >= anim.target.y - window.innerHeight * anim.viewRatio)
+					{
+						anim.started = true;
+						
+						if (anim.onEnter)
+							anim.onEnter(anim.target);
+					}
+				}
+			}
+		};
+		
+		root.scrollHandler(null);
+		window.addEventListener("scroll", root.scrollHandler.bind(root));
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1));
+
+	// anims
+	this.triangle = new lib.TriangleAnimation();
+	this.triangle.name = "triangle";
+	this.triangle.setTransform(480,4320);
+
+	this.circle = new lib.CircleAnimation();
+	this.circle.name = "circle";
+	this.circle.setTransform(480,2700);
+
+	this.square = new lib.SquareAnimation();
+	this.square.name = "square";
+	this.square.setTransform(480,1080);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.square},{t:this.circle},{t:this.triangle}]}).wait(1));
+
+	// background
+	this.instance = new lib.Background("single",0);
+	this.instance.setTransform(0,5400);
+	this.instance.alpha = 0.1992;
+
+	this.instance_1 = new lib.Background("single",0);
+	this.instance_1.setTransform(0,4320);
+	this.instance_1.alpha = 0.1992;
+
+	this.instance_2 = new lib.Background("single",0);
+	this.instance_2.setTransform(0,3240);
+	this.instance_2.alpha = 0.1992;
+
+	this.instance_3 = new lib.Background("single",0);
+	this.instance_3.setTransform(0,2160);
+	this.instance_3.alpha = 0.1992;
+
+	this.instance_4 = new lib.Background("single",0);
+	this.instance_4.setTransform(0,1080);
+	this.instance_4.alpha = 0.1992;
+
+	this.instance_5 = new lib.Background("single",0);
+	this.instance_5.setTransform(-0.05,0);
+	this.instance_5.alpha = 0.1992;
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_5},{t:this.instance_4},{t:this.instance_3},{t:this.instance_2},{t:this.instance_1},{t:this.instance}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(352,2970,608,2970);
 // library properties:
 lib.properties = {
-	id: 'B2C34A8A5C84924FB2AD8C9EC65F245D',
-	width: 640,
-	height: 480,
+	id: '3383E8FC715B0D41A43607953EA7270C',
+	width: 960,
+	height: 5940,
 	fps: 60,
-	color: "#000000",
+	color: "#00CCCC",
 	opacity: 1.00,
-	manifest: [
-		{src:"images/index_atlas_.png", id:"index_atlas_"}
-	],
+	manifest: [],
 	preloads: []
 };
 
@@ -208,7 +356,7 @@ lib.properties = {
 
 (lib.Stage = function(canvas) {
 	createjs.Stage.call(this, canvas);
-}).prototype = p = new createjs.StageGL();
+}).prototype = p = new createjs.Stage();
 
 p.setAutoPlay = function(autoPlay) {
 	this.tickEnabled = autoPlay;
@@ -235,7 +383,7 @@ an.bootstrapCallback=function(fnCallback) {
 };
 
 an.compositions = an.compositions || {};
-an.compositions['B2C34A8A5C84924FB2AD8C9EC65F245D'] = {
+an.compositions['3383E8FC715B0D41A43607953EA7270C'] = {
 	getStage: function() { return exportRoot.stage; },
 	getLibrary: function() { return lib; },
 	getSpriteSheet: function() { return ss; },
@@ -276,19 +424,27 @@ an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers)
 			else if(scaleType==2) {					
 				sRatio = Math.max(xRatio, yRatio);				
 			}			
-		}			
+		}
 		domContainers[0].width = w * pRatio * sRatio;			
-		domContainers[0].height = h * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
 		domContainers.forEach(function(container) {				
 			container.style.width = w * sRatio + 'px';				
 			container.style.height = h * sRatio + 'px';			
-		});			
+		});
 		stage.scaleX = pRatio*sRatio;			
-		stage.scaleY = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
 		lastW = iw; lastH = ih; lastS = sRatio;            
 		stage.tickOnUpdate = false;            
 		stage.update();            
 		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused){
+			stageChild.syncStreamSounds();
+		}
 	}
 }
 
